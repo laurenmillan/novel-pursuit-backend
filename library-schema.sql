4ac0ff20b9a7a562f3b376a83a09b611dd3056be
Lauren Millan <laurenmillan@mydsn.net>
2023-04-06 12:05:10 -0400

    add updated tables

--- library-schema.sql
+++ library-schema.sql
@@ -1,23 +1,23 @@
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
-    title VARCHAR,
-    author_name VARCHAR,
-    by_statement VARCHAR,
+    title TEXT,
+    author_name TEXT,
+    by_statement TEXT,
     publish_date DATE,
-    isbn VARCHAR [],
+    isbn TEXT[],
     description TEXT,
-    cover_url VARCHAR
-)
+    cover_url TEXT
+);
 
 CREATE TABLE users (
     username VARCHAR(25) PRIMARY KEY,
     password TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT NOT NULL
-    CHECK (position('@' IN email) > 1),
+    email TEXT NOT NULL CHECK (email ~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'), 
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
+
 
 CREATE TABLE bookmarks (
     username VARCHAR(25)
