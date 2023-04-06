6aeff3ab65f562c27d9d8941e4820d5ad7f5f1d4
Lauren Millan <laurenmillan@mydsn.net>
2023-04-06 11:37:37 -0400

    add tables

--- library-schema.sql
+++ library-schema.sql
@@ -1,20 +1,28 @@
 CREATE TABLE books (
-    id INT
-    author_name VARCHAR
-    title VARCHAR
-    by_statement VARCHAR
-    publish_date DATE
-    isbn VARCHAR
-    description
+    id SERIAL PRIMARY KEY,
+    title VARCHAR,
+    author_name VARCHAR,
+    by_statement VARCHAR,
+    publish_date DATE,
+    isbn VARCHAR [],
+    description TEXT,
     cover_url VARCHAR
 )
 
 CREATE TABLE users (
-  username VARCHAR(25) PRIMARY KEY,
-  password TEXT NOT NULL,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  email TEXT NOT NULL
+    username VARCHAR(25) PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL
     CHECK (position('@' IN email) > 1),
-  is_admin BOOLEAN NOT NULL DEFAULT FALSE
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE bookmarks (
+    username VARCHAR(25)
+        REFERENCES users ON DELETE CASCADE,
+    book_id INTEGER
+        REFERENCES books ON DELETE CASCADE,
+    PRIMARY KEY (username, book_id)
 );
